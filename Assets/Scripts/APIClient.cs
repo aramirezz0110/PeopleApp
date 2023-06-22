ba5f5c98a86fdaf5a5c4a49065ee3743b6da7fd8
Arom Ramirez <aramirez@shokworks.io>
2023-06-22 10:35:33 -0400

    JSON conversion working

---
+++ Assets/Scripts/APIClient.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+public class APIClient : MonoBehaviour
+{
+    [SerializeField] private string URL = "https://randomuser.me/api/?results=20";
+    public Root root;
+    private void Start()
+    {
+        StartCoroutine(GetDataCO());
+    }
+    private IEnumerator GetDataCO() 
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(URL))
+        {
+            yield return request.SendWebRequest();
+
+            if(request.result==UnityWebRequest.Result.ConnectionError)
+                Debug.Log(request.error);
+            else
+            {
+                string json = request.downloadHandler.text;
+                root = JsonConvert.DeserializeObject<Root>(json);
+                print(root.info.version);
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/JSONDataModels.cs
@@ -0,0 +1,107 @@
+// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+
+using System;
+using UnityEngine;
+
+[Serializable] public  class Coordinates
+{
+    [field:SerializeField] public string latitude { get; set; }
+    [field:SerializeField] public string longitude { get; set; }
+}
+
+[Serializable] public  class Dob
+{
+    [field:SerializeField] public DateTime date { get; set; }
+    [field:SerializeField] public int age { get; set; }
+}
+
+[Serializable] public  class Id
+{
+    [field:SerializeField] public string name { get; set; }
+    [field:SerializeField] public string value { get; set; }
+}
+
+[Serializable] public  class Info
+{
+    [field:SerializeField] public string seed { get; set; }
+    [field:SerializeField] public int results { get; set; }
+    [field:SerializeField] public int page { get; set; }
+    [field:SerializeField] public string version { get; set; }
+}
+
+[Serializable] public  class Location
+{
+    [field:SerializeField] public Street street { get; set; }
+    [field:SerializeField] public string city { get; set; }
+    [field:SerializeField] public string state { get; set; }
+    [field:SerializeField] public string country { get; set; }
+    [field:SerializeField] public object postcode { get; set; }
+    [field:SerializeField] public Coordinates coordinates { get; set; }
+    [field:SerializeField] public Timezone timezone { get; set; }
+}
+
+[Serializable] public  class Login
+{
+    [field:SerializeField] public string uuid { get; set; }
+    [field:SerializeField] public string username { get; set; }
+    [field:SerializeField] public string password { get; set; }
+    [field:SerializeField] public string salt { get; set; }
+    [field:SerializeField] public string md5 { get; set; }
+    [field:SerializeField] public string sha1 { get; set; }
+    [field:SerializeField] public string sha256 { get; set; }
+}
+
+[Serializable] public  class Name
+{
+    [field:SerializeField] public string title { get; set; }
+    [field:SerializeField] public string first { get; set; }
+    [field:SerializeField] public string last { get; set; }
+}
+
+[Serializable] public  class Picture
+{
+    [field:SerializeField] public string large { get; set; }
+    [field:SerializeField] public string medium { get; set; }
+    [field:SerializeField] public string thumbnail { get; set; }
+}
+
+[Serializable] public  class Registered
+{
+    [field:SerializeField] public DateTime date { get; set; }
+    [field:SerializeField] public int age { get; set; }
+}
+
+[Serializable] public  class Result
+{
+    [field:SerializeField] public string gender { get; set; }
+    [field:SerializeField] public Name name { get; set; }
+    [field:SerializeField] public Location location { get; set; }
+    [field:SerializeField] public string email { get; set; }
+    [field:SerializeField] public Login login { get; set; }
+    [field:SerializeField] public Dob dob { get; set; }
+    [field:SerializeField] public Registered registered { get; set; }
+    [field:SerializeField] public string phone { get; set; }
+    [field:SerializeField] public string cell { get; set; }
+    [field:SerializeField] public Id id { get; set; }
+    [field:SerializeField] public Picture picture { get; set; }
+    [field:SerializeField] public string nat { get; set; }
+}
+
+[Serializable] public  class Root
+{
+    [field:SerializeField] public Result[] results { get; set; }
+    [field:SerializeField] public Info info { get; set; }
+}
+
+[Serializable] public  class Street
+{
+    [field:SerializeField] public int number { get; set; }
+    [field:SerializeField] public string name { get; set; }
+}
+
+[Serializable] public  class Timezone
+{
+    [field:SerializeField] public string offset { get; set; }
+    [field:SerializeField] public string description { get; set; }
+}
+
